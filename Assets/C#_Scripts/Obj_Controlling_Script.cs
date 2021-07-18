255e462d038078b9e09fa0c60d990191f5c6b08c
Northman94 <Zhandarm2000@gmail.com>
2021-07-18 22:45:25 +0300

    Obj_Controlling_Script

---
+++ Assets/C#_Scripts/Obj_Controlling_Script.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Obj_Controlling_Script : MonoBehaviour
+{
+
+    [SerializeField]
+    int moveSpeed;   //Set in Inspector
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+    }
+}
--- Assets/C#_Scripts/Obj_Spawn_Script.cs
+++ Assets/C#_Scripts/Obj_Spawn_Script.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Obj_Spawn_Script : MonoBehaviour
