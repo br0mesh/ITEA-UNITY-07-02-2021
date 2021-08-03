bc5423fdb0cdc3d72fa199df4e48e70c80672569
S-GENESlS-D <danilins.prog@gmail.com>
2021-08-03 22:22:09 +0300

    -1

---
+++ Sergey-Danilin-lesson-3/Assets/Scipts/MapGenerator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private int groundSize = 100;
+
+    [SerializeField] private GameObject groundHolder;
+
+    [SerializeField] private GameObject characterPrefab;
+
+    private void Start()
+    {
+        GenerateGround();
+        SpawnCharacter();
+    }
+    private void GenerateGround()
+    {
+        for (int i = 0; i < groundSize; i++)
+        {
+            GameObject groundTile = Instantiate(tilePrefab,groundHolder.transform);
+
+            groundTile.transform.position = new Vector3(-groundSize / 2 + i, 0, 0);
+        }
+
+    }
+    private void SpawnCharacter()
+    {
+        Instantiate(characterPrefab);
+    }
+}
